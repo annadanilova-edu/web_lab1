cdc7925e16811bfa8c62128932824ea3ea008b8f
Anna <weinxor@gmail.com>
2019-09-18 09:15:43 +0300

    Adding Mustache

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
+
 let key = "bf17aa753eec52f73cdb8d53e0609031"
 
+
 function getWeather(){
+    let city = document.getElementById("city-input")
     let request = new XMLHttpRequest();
 
-    let city = document.getElementById("city-input").value
-    let link = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key;
+    let link = "https://api.openweathermap.org/data/2.5/weather?q=" + city.value + "&appid=" + key;
 
     request.open("GET", link, false); // true for asynchronous
     request.send();
 
     let jsonObject = JSON.parse(request.responseText);
-    alert(jsonObject.name);
+    city.innerHTML = jsonObject.name;
+
+    let template = "{{base}}";
+
+    document.getElementById("result").innerText = Mustache.render(template, jsonObject);
 }
+
