f4a4b9adbb003838b1f2ae50d44cd3d6935f1099
Anna <weinxor@gmail.com>
2019-09-29 19:04:14 +0300

    Adding Mustache

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
 
-let key = "bf17aa753eec52f73cdb8d53e0609031"
+document.addEventListener("DOMContentLoaded", ready);
+let city;
+let name, temp, wind, weather, humidity;
+const key = "bf17aa753eec52f73cdb8d53e0609031";
 
+function ready(){
+    city = document.getElementById("search-txt");
+    name = document.getElementById("name");
+    temp = document.getElementById("temp");
+    weather = document.getElementById("weather");
+    wind = document.getElementById("wind");
+    humidity = document.getElementById("humidity");
 
+    let searchButton = document.getElementById("search-btn");
+    let searchInput = document.getElementById("search-txt");
+
+    searchButton.addEventListener("click", getWeather);
+    searchInput.addEventListener("keyup", enterPressed);
+}
+function enterPressed(event) {
+    if (event.key === "Enter") {
+        getWeather();
+    }
+}
 function getWeather(){
-    let city = document.getElementById("city-input")
-    let request = new XMLHttpRequest();
-
-    let link = "https://api.openweathermap.org/data/2.5/weather?q=" + city.value + "&appid=" + key;
-
-    request.open("GET", link, false); // true for asynchronous
-    request.send();
-
-    let jsonObject = JSON.parse(request.responseText);
-    city.innerHTML = jsonObject.name;
-
-    let template = "{{base}}";
-
-    document.getElementById("result").innerText = Mustache.render(template, jsonObject);
+    let link = "https://api.openweathermap.org/data/2.5/weather?q=" + city.value + "&units=metric" + "&appid=" + key;
+    fetch(link)
+        .then(function(response){
+            if (response.ok){
+                return response.json()
+            }
+            else {
+                alert("Ошибка HTTP: " + response.status);
+                name.innerText = temp.innetText = wind.innerText = weather.innerText = humidity.innerText = "";
+            }
+        })
+        .then(function(json) {
+            console.log(json);
+            name.innerHTML = Mustache.render("{{name}}", json);
+            temp.innerHTML = Mustache.render("{{main.temp}}°C", json);
+            wind.innerHTML = Mustache.render("Wind:</br> {{wind.speed}} meter/sec", json);
+            weather.innerHTML = Mustache.render("{{weather.0.main}}", json);
+            humidity.innerHTML = Mustache.render("Humidity:</br> {{main.humidity}}%", json);
+        });
 }
 
