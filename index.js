471d3d79ee6a4eca632c58604dc547330751c550
Anna <weinxor@gmail.com>
2019-10-03 14:23:27 +0300

    Small fixes

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const API_KEY = "bf17aa753eec52f73cdb8d53e0609031";
 
 function getWeather() {
     let grid = document.getElementById("grid");
-    let link = "https://api.openweathermap.org/data/2.5/weather?q=" + event.target[0].value + "&units=metric" + "&appid=" + API_KEY;
     let warning = document.getElementById("warning");
+    let link = "https://api.openweathermap.org/data/2.5/weather?q="
+        + event.target['search-txt'].value
+        + "&units=metric"
+        + "&appid=" + API_KEY;
 
     fetch(link)
         .then(function (response) {
@@ -15,12 +18,12 @@ function getWeather() {
             }
         })
         .then(function (json) {
-            drawWeather(grid, json);
+            drawWeather(grid, json, warning);
         });
 
 }
 
-function drawWeather(grid, json) {
+function drawWeather(grid, json, warning) {
     clearForm(grid);
     let template = '<div class="name" id="name">{{name}}</div>\n' +
         '    <div class="temp" id="temp">{{main.temp}}°C</div>\n' +
@@ -29,7 +32,7 @@ function drawWeather(grid, json) {
         '    <div class="humidity" id="humidity">Humidity:</br> {{main.humidity}}%</div>';
 
     if (json) {
-        document.getElementById("warning").style.visibility = "hidden";
+        warning.style.visibility = "hidden";
         grid.innerHTML += Mustache.render(template, json);
     }
 }
